0a97c582d04e976a1a667ceaa7ed46d3ede2c329
TranThai <trandbthai@gmail.com>
2021-05-07 00:12:32 -0400

    Topic 11 User Interaction Part II

--- selenium/webdriver/api/Topic_11_User_Interaction_Part_II.java
+++ selenium/webdriver/api/Topic_11_User_Interaction_Part_II.java
@@ -1,10 +1,17 @@
 package webdriver.api;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -20,12 +27,17 @@ import org.openqa.selenium.support.Color;
 public class Topic_11_User_Interaction_Part_II {
 	WebDriver driver;
 	Actions action;
+	String projectPath = System.getProperty("user.dir");
+	JavascriptExecutor javascriptExecutor;
+	String javascriptPath = projectPath + "/dragAndDrop/drag_and_drop_helper.js";
+	String jqueryPath = projectPath + "/dragAndDrop/query_load_helper.js";
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", "./browserDriver/geckodriver");		
+		System.setProperty("webdriver.gecko.driver", "./browserDriver/geckodriver");
 		driver = new FirefoxDriver();
 		action = new Actions(driver);
+		javascriptExecutor = (JavascriptExecutor) driver;
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -60,7 +72,7 @@ public class Topic_11_User_Interaction_Part_II {
 		Assert.assertFalse(driver.findElement(By.xpath("//li[contains(@class,'context-menu-icon-quit')]")).isDisplayed());
 	}
 	
-	@Test
+
 	public void TC_02_Drag_And_Drop_HTML4() {
 		driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
 		
@@ -76,21 +88,97 @@ public class Topic_11_User_Interaction_Part_II {
 		
 	}
 
-	
-	public void TC_03_Drag_And_Drop_HTML5() {
+
+	public void TC_03_Drag_And_Drop_HTML5_CSS() throws IOException {
 		driver.get("https://automationfc.github.io/drag-drop-html5/");
-		
-		WebElement sourceRectangle = driver.findElement(By.id("column-a"));
-		WebElement targetRectangle = driver.findElement(By.id("column-b"));
-		
-		action.dragAndDrop(sourceRectangle, targetRectangle).perform();
+
+		String sourceCss = "#column-a";
+		String targetCss = "#column-b";
+
+		String java_script = getFileContent(javascriptPath);
+
+		// Inject Jquery lib to site
+		// String jqueryscript = readFile(jqueryPath);
+		//bai nay ko dung jquery vi trong site co san jquery
+		// javascriptExecutor.executeScript(jqueryscript);
+
+		// A to B
+		java_script = java_script + "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
+		javascriptExecutor.executeScript(java_script);
 		sleepInSeconds(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='B']")).isDisplayed());
+
+		// B to A
+		javascriptExecutor.executeScript(java_script);
+		sleepInSeconds(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='B']")).isDisplayed());
 		
 		
 		
 	}
-	
-	
+
+	@Test
+	public void TC_03_Drag_And_Drop_HTML5_Xpath() throws AWTException {
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+
+
+		drag_the_and_drop_html5_by_xpath("//div[@id='column-a']","//div[@id='column-b']");
+		sleepInSeconds(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='B']")).isDisplayed());
+
+		drag_the_and_drop_html5_by_xpath("//div[@id='column-a']","//div[@id='column-b']");
+		sleepInSeconds(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='B']")).isDisplayed());
+	}
+
+
+	public void drag_the_and_drop_html5_by_xpath(String sourceLocator, String targetLocator) throws AWTException {
+
+		WebElement source = driver.findElement(By.xpath(sourceLocator));
+		WebElement target = driver.findElement(By.xpath(targetLocator));
+
+		// Setup robot
+		Robot robot = new Robot();
+		robot.setAutoDelay(500);
+
+		// Get size of elements
+		org.openqa.selenium.Dimension sourceSize = source.getSize();
+		org.openqa.selenium.Dimension targetSize = target.getSize();
+
+		// Get center distance
+		int xCentreSource = sourceSize.width / 2;
+		int yCentreSource = sourceSize.height / 2;
+		int xCentreTarget = targetSize.width / 2;
+		int yCentreTarget = targetSize.height / 2;
+
+		org.openqa.selenium.Point sourceLocation = source.getLocation();
+		Point targetLocation = target.getLocation();
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Make Mouse coordinate center of element
+		sourceLocation.x += 20 + xCentreSource;
+		sourceLocation.y += 110 + yCentreSource;
+		targetLocation.x += 20 + xCentreTarget;
+		targetLocation.y += 110 + yCentreTarget;
+
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Move mouse to drag from location
+		robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+		// Click and drag
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+		// Move to final position
+		robot.mouseMove(targetLocation.x, targetLocation.y);
+
+		// Drop
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+
 	public void sleepInSeconds(long timeout) {
 		try {
 			Thread.sleep(timeout * 1000);
@@ -107,8 +195,23 @@ public class Topic_11_User_Interaction_Part_II {
 	public String getHexValue(String rgbValue) {
 		return Color.fromString(rgbValue).asHex();
 	}
-	
-	
+
+	public String getFileContent(String file) throws IOException {
+		Charset cs = Charset.forName("UTF-8");
+		FileInputStream stream = new FileInputStream(file);
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+			StringBuilder builder = new StringBuilder();
+			char[] buffer = new char[8192];
+			int read;
+			while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			return builder.toString();
+		} finally {
+			stream.close();
+		}
+	}
 
 	@AfterClass
 	public void afterClass() {
